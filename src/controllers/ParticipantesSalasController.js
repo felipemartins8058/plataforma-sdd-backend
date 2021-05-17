19bea368b0aad6d1f29331450a47fb727938f37e
Gabriely Rodrigues <gabriely.bleez@gmail.com>
2021-05-17 20:29:54 -0300

    verificando se cargo selecionado já existe naquela sala

--- src/controllers/ParticipantesSalasController.js
+++ src/controllers/ParticipantesSalasController.js
@@ -13,14 +13,25 @@ module.exports = {
         const { sala_id } = request.params;
         const { nome, cargo_id } = request.body;
 
+        const cargo_existente = await connection('participantes_salas')
+            .select('participantes_salas.cargo_id')
+            .where('participantes_salas.cargo_id', cargo_id)
+            .andWhere('participantes_salas.sala_id', sala_id)
+            .first();
+
         try {	
-            const participante_sala = await connection('participantes_salas').insert({
-                nome,
-                cargo_id,
-                sala_id,
-            });
-            
-            response.status(200).json(participante_sala);
+            if(!cargo_existente) { //se o cargo selecionado não tiver sido cadastrado ainda naquela sala ele cadastra
+                const participante_sala = await connection('participantes_salas').insert({
+                    nome,
+                    cargo_id,
+                    sala_id,
+                });
+    
+                response.status(200).json(participante_sala);
+            }
+
+            // se já existe ele dá erro
+            response.status(500).json({ message: "Cargo já existente na sala" });
         } catch (error) {
             response.status(500).json({ error: error });
         }
